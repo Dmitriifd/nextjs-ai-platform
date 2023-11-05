b5aba2652c169818258fb458ef546312fee34b0c
Dmitriifd <idimka9@gmail.com>
2023-11-05 14:40:13 +0500

    clerk auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div>
+      DashboardPage
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
 
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,21 @@
+import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div>
+      LandingPage
+      <div className='flex gap-4'>
+        <Link href='/sign-in'>
+          <Button variant='destructive'>Login</Button>
+        </Link>
+        <Link href='/sign-up'>
+          <Button variant='destructive'>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
