452ed94a70e36668294271748fc639b6b28441da
Dmitriifd <idimka9@gmail.com>
2023-11-06 19:16:42 +0500

    LandingPage

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,19 +1,13 @@
-import { Button } from '@/components/ui/button';
-import { UserButton } from '@clerk/nextjs';
-import Link from 'next/link';
+import { LandingNavbar } from '@/components/landing-navbar';
+import { LandingHero } from '@/components/landing-hero';
+import { LandingContent } from '@/components/landing-content';
 
 const LandingPage = () => {
   return (
-    <div>
-      LandingPage
-      <div className='flex gap-4'>
-        <Link href='/sign-in'>
-          <Button variant='destructive'>Login</Button>
-        </Link>
-        <Link href='/sign-up'>
-          <Button variant='destructive'>Register</Button>
-        </Link>
-      </div>
+    <div className='h-full '>
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
